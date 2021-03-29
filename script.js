126a26983d9fbff6b84e74bee8923f34202fd6f1
Jason Hedrick <jasonleehedrick@gmail.com>
2021-03-29 12:22:00 -0700

    Basic functionality of the counter added.

---
+++ script.js
@@ -0,0 +1,24 @@
+const decrement = document.querySelector('#decrement');
+const increment = document.querySelector('#increment');
+const reset = document.querySelector('#reset');
+const counter = document.querySelector('#counter');
+let count = 0;
+
+decrement.addEventListener('click', () => {
+    count--;
+    setCounter(count);
+});
+
+increment.addEventListener('click', () => {
+    count++;
+    setCounter(count);
+});
+
+reset.addEventListener('click', () => {
+    count = 0;
+    setCounter(count);
+});
+
+function setCounter(value) {
+    counter.innerHTML = value;
+}
