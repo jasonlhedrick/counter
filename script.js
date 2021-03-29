48a07cadedb4e546e04ab77938c99b3962b10344
Jason Hedrick <jasonleehedrick@gmail.com>
2021-03-29 12:55:03 -0700

    Added an input which allows the user to change how much they are changing the value by.

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const decrement = document.querySelector('#decrement');
 const increment = document.querySelector('#increment');
 const reset = document.querySelector('#reset');
 const counter = document.querySelector('#counter');
+const valueChange = document.querySelector('#valueChange');
+let changeQuantity = parseInt(valueChange.value);
 let count = 0;
 
+valueChange.addEventListener('input', () => {
+    changeQuantity = parseInt(valueChange.value);
+});
+
 decrement.addEventListener('click', () => {
-    count--;
+    count -= changeQuantity;
     setCounter(count);
 });
 
 increment.addEventListener('click', () => {
-    count++;
+    count += changeQuantity;
     setCounter(count);
 });
 
